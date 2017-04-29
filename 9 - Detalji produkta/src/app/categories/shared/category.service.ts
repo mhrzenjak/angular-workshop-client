82f7797fe4cf3a35283e49bbe186fa00a79eded6
Matija Hrženjak <matija.hrzenjak@hotmail.com>
2017-04-29 13:15:35 +0200

    Završeni detalji produkta

---
+++ 9 - Detalji produkta/src/app/categories/shared/category.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+
+import { CategoryListModel } from "../category-list/category-list.model";
+import { ProductSummaryModel } from "../../products/product-summary/product-summary.model";
+
+@Injectable()
+export class CategoryService {
+
+    getCategories(): Observable<Array<CategoryListModel>>{
+        var categoryList: Array<CategoryListModel> = [
+            { id: 1, text: "Rock" },
+            { id: 2, text: "Punk" },
+            { id: 3, text: "Pop" },
+            { id: 4, text: "Techno" },
+            { id: 5, text: "House" },
+            { id: 6, text: "Klasična glazba" },
+            { id: 7, text: "Ostalo" }
+        ];
+        return Observable.of(categoryList);
+    }
+
+    getCategory(id: number): Observable<Array<ProductSummaryModel>>{
+        return null;
+    }
+}
