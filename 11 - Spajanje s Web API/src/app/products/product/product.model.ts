a5c4a5c8095f2c2dd15bd65de54008e4b0bd79eb
Matija Hrženjak <matija.hrzenjak@hotmail.com>
2017-04-30 11:33:13 +0200

    Završena integracija s Web API-em

---
+++ 11 - Spajanje s Web API/src/app/products/product/product.model.ts
@@ -0,0 +1,6 @@
+import { ProductSummaryModel } from "../product-summary/product-summary.model";
+
+export class ProductModel extends ProductSummaryModel{
+     albumName: string;
+     duration: number;
+}
