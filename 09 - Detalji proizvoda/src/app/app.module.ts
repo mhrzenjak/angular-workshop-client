69fe3a9e64a8f49ff813d84ef368e29feb62815d
Matija Hrženjak <matija.hrzenjak@hotmail.com>
2017-05-01 09:11:13 +0200

    Update detalji proizvoda

--- 09 - Detalji proizvoda/src/app/app.module.ts
+++ 09 - Detalji proizvoda/src/app/app.module.ts
@@ -7,21 +7,28 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { CategoryListComponent } from './categories/category-list/category-list.component';
 import { CategoryComponent } from './categories/category/category.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProductSummaryComponent } from "./products/product-summary/product-summary.component";
 import { ProductComponent } from "./products/product/product.component";
 
 import { CategoryService } from "./categories/shared/category.service";
-import { ProductService } from "./products/shared/product.service";
+import { I_CATEGORY_SERVICE } from "./categories/shared/icategory.service";
+import { I_PRODUCT_SERVICE } from "./products/shared/iproduct.service";
 
 import { InMemoryDataService } from './testing/in-memory-data.service';
 import { CategoryMockService } from "./testing/category-mock.service";
 import { ProductMockService } from "./testing/product-mock.service";
 
 @NgModule({
-  imports: [BrowserModule,
+  imports: [
+    BrowserModule,
     HttpModule,
     InMemoryWebApiModule.forRoot(InMemoryDataService),
     RouterModule.forRoot([
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
       {
         path: 'category/:id',
         component: CategoryComponent
@@ -29,14 +36,19 @@ import { ProductMockService } from "./testing/product-mock.service";
       {
         path: 'product/:id',
         component: ProductComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
       }
     ])
   ],
   providers: [
-    { provide: CategoryService, useClass: CategoryMockService },
-    { provide: ProductService, useClass: ProductMockService }
+    { provide: I_CATEGORY_SERVICE, useClass: CategoryMockService },
+    { provide: I_PRODUCT_SERVICE, useClass: ProductMockService }
   ],
-  declarations: [AppComponent, CategoryListComponent, CategoryComponent, ProductSummaryComponent, ProductComponent],
+  declarations: [AppComponent, CategoryListComponent, CategoryComponent, DashboardComponent, ProductSummaryComponent, ProductComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- 09 - Detalji proizvoda/src/app/products/product/product.component.ts
+++ 09 - Detalji proizvoda/src/app/products/product/product.component.ts
@@ -1,32 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
 import { ProductModel } from "./product.model";
-import { ProductService } from "../shared/product.service";
+
+import { I_PRODUCT_SERVICE, IProductService } from "../shared/iproduct.service";
 
 @Component({
     selector: 'product',
     templateUrl: './product.component.html'
 })
 export class ProductComponent {
-    id: number;
+
     product: ProductModel;
 
     constructor(
         private route: ActivatedRoute,
-        private productService: ProductService
-        ) { }
+        @Inject(I_PRODUCT_SERVICE) private productService: IProductService,
+    ) { }
 
     ngOnInit(): void {
+
         this.route.params.subscribe(
             params => {
-                this.id = params["id"];
-                this.getProduct(this.id);
+                this.getProduct(params["id"]);
             }
         );
     }
 
     getProduct(id: number){
+
         this.productService.getProduct(id).subscribe(
             value => {
                 this.product = value;
